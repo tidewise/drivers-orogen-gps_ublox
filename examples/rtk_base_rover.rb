24f565016c5980ce1b9a74e3139de54df0b531f2
Sylvain <sylvain.joyeux@tidewise.io>
2022-03-28 14:55:53 -0300

    feat: use config files to configure the examples

--- examples/rtk_base_rover.rb
+++ examples/rtk_base_rover.rb
@@ -1,37 +1,18 @@
 using_task_library "gps_ublox"
 
-Syskit.extend_model OroGen.gps_ublox.Task do
-    argument :io_port
-
-    def update_properties
-        super
-
-        properties.io_port = io_port
-        properties.rtcm_output_messages = [1074, 1084, 1094, 1124, 1230, 4072]
-        properties.msg_rates do |r|
-            r.rtk_info = 1
-            r.nav_relposned = 1
-            r
-        end
-    end
-end
-
 module RTK
     class BaseRover < Syskit::Composition
         add(OroGen.gps_ublox.Task, as: "reference")
-            .with_arguments(io_port: ENV["REFERENCE_IO"])
-            .prefer_deployed_tasks("reference")
+            .deployed_as("ublox_reference_station")
+            .with_conf("default", "reference")
         add(OroGen.gps_ublox.Task, as: "rover")
-            .with_arguments(io_port: ENV["ROVER_IO"])
-            .prefer_deployed_tasks("rover")
+            .deployed_as("ublox_rover")
+            .with_conf("default", "rover")
 
         reference_child.rtcm_out_port.connect_to \
             rover_child.rtcm_in_port, type: :buffer, size: 100
     end
 end
-
-Syskit.conf.use_deployment OroGen.gps_ublox.Task => "reference"
-Syskit.conf.use_deployment OroGen.gps_ublox.Task => "rover"
 
 Robot.controller do
     Roby.plan.add_mission_task RTK::BaseRover
